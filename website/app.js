09da6bb010ece6d8a1efa3f183ae3c553163b5cb
Maksim Chekryshov <m.chekryshov@rambler-co.ru>
2015-07-28 17:36:27 +0300

    install tracekit

--- website/app.js
+++ website/app.js
@@ -12,7 +12,26 @@
         var mainModel = new MainModel({startDate: this.parseDate(appConfig.startDate)});
         var mainView = new MainView({model: mainModel});
 
-        $('#mainContainer').html(mainView.render().$el.html());
+        $('#mainContainer')
+            .append(mainView.render()
+                .$el
+                .click(onMainViewClick));
+
+        function onMainViewClick() {
+            try {
+                /*
+                 * your application code here
+                 *
+                 */
+                throw new Error('oops');
+            } catch (e) {
+                TraceKit.report(e); //error with stack trace gets normalized and sent to subscriber
+            }
+        }
+    },
+
+    logError: function(errorReport){
+        console.log(errorReport);
     }
 
 });
@@ -20,3 +39,5 @@
 var app = new AppRouter();
 
 Backbone.history.start();
+
+TraceKit.report.subscribe(app.logError);
