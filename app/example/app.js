b65d040e6a7dfc5771e02d5ee4b15ace41d38d0b
Maksim Chekryshov <m.chekryshov@rambler-co.ru>
2015-08-07 19:45:06 +0300

    reformat index page

--- app/example/app.js
+++ app/example/app.js
@@ -3,46 +3,15 @@
  * Created by m.chekryshov on 30.07.15.
  */
 
-(function() {
+var $ = require('jquery');
 
-    $('.js-handled-error-button').click(function() {
-        handledError();
+Sneaker.installJqueryAjaxErrorHandler($);
+
+
+$(document).ready(function() {
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
     });
-    $('.js-unhandled-error-button').click(function() {
-        unhandledError();
-    });
-    $('.js-wrapped-method-error-button').click(function() {
-        wrapedMethodError();
-    });
+});
 
-    $('.js-error-button').click(function() {
-        Sneaker.error('Custom error');
-    });
-    $('.js-warning-button').click(function() {
-        Sneaker.warn('Some warning info');
-    });
-    $('.js-info-button').click(function() {
-        Sneaker.info('Some inforamtion');
-    });
-
-    function funcToWrap() {
-        throw new Error('Error from wrapped method');
-    }
-
-    function unhandledError() {
-        throw new Error('Unhandled Error');
-    }
-
-    function handledError() {
-        try {
-            throw new Error('Some handled Error');
-        } catch (e) {
-            Sneaker.handleException(e);
-        }
-    }
-
-    function wrapedMethodError() {
-        Sneaker.callThrough(funcToWrap)
-    }
-
-})();
+require('./modules/mainModule');
